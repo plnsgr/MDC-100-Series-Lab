9ec0c6bf9baf4015842c8a2b78c03afeffd1b549
OS1RIS <38433039+plnsgr@users.noreply.github.com>
2023-11-10 19:21:10 +0900

    Add files via upload

---
+++ MDC-102/lib/login.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Text editing controllers for username and password
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                const SizedBox(height: 16.0),
+                const Text('SHRINE'),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            // Text field for username
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0), // Spacer
+            // Text field for password
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            // Button bar
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              children: <Widget>[
+                // Cancel button
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    // Clear the text fields
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                // Next button
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
